85511005588cb8ade62d9e2287db53ca6916e337
Iza Evans <iza_evans@hotmail.com>
2025-09-05 23:53:07 -0300

    Finalização do básico do aplicativo - remover e selecionar ítens da lista

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +9,30 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+
+    private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
